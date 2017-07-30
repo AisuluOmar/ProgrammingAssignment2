6728c616242bb28317baf2307b367b60865022ea
AisuluOmar <aisuluomar@yahoo.com>
2017-07-30 13:48:41 +0600

    Assignment 2 solution

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -22,29 +22,17 @@ Type 'q()' to quit R.
 [Workspace restored from /Users/aisuluomar/.RData]
 [History restored from /Users/aisuluomar/.Rapp.history]
 
-> makeCacheMatrix <- function(x = matrix()) {
-+ inv <- function(y) {
-+ x <<- y
-+ inv <<- NULL
-+ }
-+ get <- function () x
-+ setInverse <- function(inverse) inv <<- inverse
-+ getInverse <- function() inv
-+ list(set = set,
-+ get = get,
-+ setInverse = setInverse,
-+ getInverse = getInverse)
-+ }
-
-> cacheSalve <- function(x, ...) {
-+ inv <- x$getInverse()
-+ if(!is.null(inv)) {
-+ message("getting cached data")
-+ return(inv)
-+ }
-+ mat <- x$get()
-+ inv <- solve(mat, ...)
-+ x$setInverse(inv)
-+ inv
-+ }
-> 
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+            x <<- y
+            inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+} 
